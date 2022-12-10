2dea6a02b19fc5a85c447b5d083c8d603446135b
vtrsz <vitorsouzaribeiro1@gmail.com>
2022-12-09 21:23:15 -0300

    feat: Spring project base

---
+++ src/main/java/com/mesttra/pecasapi/PecasApiApplication.java
@@ -0,0 +1,13 @@
+package com.mesttra.pecasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mesttra/pecasapi/PecasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesttra.pecasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
